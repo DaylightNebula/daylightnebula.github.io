87fb82cad390c3df7f02032c713f67ff709709de
DaylightNebula <35437205+DaylightNebula@users.noreply.github.com>
2023-04-04 23:35:30 -0600

    Getting ready for github api stuffs

--- src/elements/RepositoriesElement.js
+++ src/elements/RepositoriesElement.js
@@ -1,20 +1,78 @@
-import {Container, FormText} from "react-bootstrap";
+import {Container, FloatingLabel, Form, FormText} from "react-bootstrap";
 import {colors} from "../helpers/Colors";
+import BetterButton from "../helpers/BetterButton";
+import BetterText from "../helpers/BetterText";
+import {BsFillCaretDownFill, BsSearch} from "react-icons/bs";
+
+function Filter() {
+    return (
+        <Container style={{
+            margin: 5,
+            background: `linear-gradient(to right, ${colors.primaries.green}, ${colors.primaries.purple})`,
+            borderRadius: 100,
+            boxShadow: `0 5px 2px rgba(0, 0, 0, 0.1)`
+        }}>
+            <BetterButton initColor={"transparent"} hoverColor={"rgba(1, 1, 1, 0.1)"} radius={100} padding={20} >
+                <BetterText color={colors.grays.offwhite} cursor="pointer" >
+                    <Container style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                        Filter
+                        <div style={{ width: 10 }} />
+
+                        {/* fix spacing issues with caret */}
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <div style={{ height: 5 }} />
+                            <BsFillCaretDownFill/>
+                        </div>
+                    </Container>
+                </BetterText>
+            </BetterButton>
+        </Container>
+    );
+}
+
+function Search() {
+    return (
+        <Container style={{
+            margin: 5,
+            borderRadius: 100,
+            boxShadow: `0 5px 2px rgba(0, 0, 0, 0.1)`
+        }}>
+            <BetterButton initColor={colors.grays.lightgray} hoverColor={colors.grays.midgray} radius={100} padding={20} >
+                <Container style={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+                    <BsSearch style={{ width: 20, height: 20 }} />
+                    <div style={{ width: 10 }} />
+                    <Form.Control style={{
+                        border: "none",
+                        background: "transparent",
+                        overflow: "auto",
+                        outline: "none",
+                        boxShadow: "none",
+                        resize: "none",
+                        rows: "1",
+                        fontFamily: "Roboto Mono",
+                        fontSize: 20
+                    }} placeholder="Search..." />
+                </Container>
+            </BetterButton>
+        </Container>
+    );
+}
 
 export default function RepositoriesElement() {
     return (
         <Container style={{
             minHeight: "100vh",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: colors.grays.offwhite
+            background: colors.grays.offwhite,
+            flexDirection: "column"
         }} >
-            <FormText style={{
-                fontFamily: "Roboto Mono",
-                fontSize: 50,
-                color: colors.grays.darkgray
-            }} >TODO Repositories</FormText>
+            <Container style={{
+                display: "flex",
+                flexDirection: "row"
+            }}>
+                <Filter/>
+                <Search/>
+            </Container>
         </Container>
     );
 }
--- src/elements/Topbar.js
+++ src/elements/Topbar.js
@@ -15,7 +15,7 @@ function TopbarElement(props) {
 
     return (
         // eslint-disable-next-line no-restricted-globals
-        <BetterButton initColor={props.isHighlighted ? colors.grays.offwhite : "transparent"} hoverColor={props.isHighlighted ? colors.grays.offwhite : colors.grays.campfire} radius={100} onClick={goto} >
+        <BetterButton initColor={props.isHighlighted ? colors.grays.offwhite : "transparent"} hoverColor={props.isHighlighted ? colors.grays.offwhite : "rgba(1, 1, 1, 0.1)"} radius={100} onClick={goto} >
             <BetterText cursor={"pointer"} color={props.isHighlighted ? colors.grays.darkgray : colors.grays.offwhite} padding={5} >
                 {props.children}
             </BetterText>
@@ -24,7 +24,7 @@ function TopbarElement(props) {
 }
 
 function Topbar() {
-    // setup scroll state so we know which state we are in
+    // setup scroll state, so we know which state we are in
     const [scroll, setScroll] = useState(0);
     useEffect(() => {
         window.addEventListener("scroll", (e) => setScroll(Math.round(window.scrollY / window.innerHeight)))
@@ -40,7 +40,8 @@ function Topbar() {
             display: "flex",
             flexDirection: "row",
             maxWidth: '100%',
-            backgroundImage: `linear-gradient(to right, ${colors.primaries.green}, ${colors.primaries.purple})`
+            backgroundImage: `linear-gradient(to right, ${colors.primaries.green}, ${colors.primaries.purple})`,
+            boxShadow: `0 5px 2px rgba(0, 0, 0, 0.1)`
         }}>
             <TopbarElement scrollMult={0} isHighlighted={scroll === 0} >Introduction</TopbarElement>
             <div style={{ width: 10 }} />
@@ -48,7 +49,7 @@ function Topbar() {
             <div style={{ width: 10 }} />
             <TopbarElement scrollMult={2} isHighlighted={scroll === 2} >Projects</TopbarElement>
             <div style={{ width: 10 }} />
-            <TopbarElement scrollMult={3} isHighlighted={scroll === 3} >Repositories</TopbarElement>
+            <TopbarElement scrollMult={3} isHighlighted={scroll === 3} >Releases</TopbarElement>
         </Container>
     );
 }
